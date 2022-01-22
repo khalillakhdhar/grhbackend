2f50615ba2b6edc6e9e87c14acc740f7ce359a4b
Marwasouilah <92760285+Marwasouilah@users.noreply.github.com>
2022-01-22 12:26:08 +0100

    Update conge.model.js

--- app/models/conge.model.js
+++ app/models/conge.model.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const CongeSchema= mongoose.Schema(
 {
+titre:String,
+duree:String,
+date:String,
+raison:String,
+justification:String,
 
 });
 module.exports= mongoose.model("conge",CongeSchema);
