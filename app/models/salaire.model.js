82c37dbc22f96ee7dbbb98834c13ed2850dae500
seddik2022 <95368704+seddik2022@users.noreply.github.com>
2022-01-22 12:14:43 +0100

    Update salaire.model.js

---
+++ app/models/salaire.model.js
@@ -0,0 +1,12 @@
+<<<<<<< Updated upstream
+=======
+const mongoose = require("mongoose");
+const SalaireSchema= mongoose.Schema(
+{
+nette: Number,
+brutte: Number,
+taxes: Number
+
+});
+module.exports= mongoose.model("salare",SalaireSchema);
+>>>>>>> Stashed changes
