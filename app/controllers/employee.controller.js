2fbff54a80d5cb6f2bc5da4ce3cadf472dc16df9
khalil <khalillakhdharatc@gmail.com>
2022-01-23 15:23:51 +0100

    Update employee.controller.js

---
+++ app/controllers/employee.controller.js
@@ -0,0 +1,25 @@
+const empSc=require("../models/employee.model");
+exports.ajouteremp=(req,res)=>
+{
+const employee=new empSc(
+    {
+        nom: req.body.nom,
+        email: req.body.email,
+        mdp: req.body.mdp,
+        tel: req.body.tel,
+        grade: req.body.grade 
+    }
+);
+employee.save()
+.then( (data)=>{
+    res.send(data);
+})
+.catch((err) => {
+    res.status(500).send({
+      message:
+        err.message || "Some error occurred while creating the Message.",
+    });
+  });
+
+  
+}
